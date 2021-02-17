badf8998565c0d662c21437636e5e46d21b02819
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-17 16:49:50 -0300

    Create Desafio 03.py

---
+++ Aula 04/Desafio 03.py
@@ -0,0 +1,6 @@
+#Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+
+print(f'A soma dos dois números é de {a + b}')
