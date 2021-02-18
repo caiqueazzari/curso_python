0958c39aefe9fd017e03ae823fa4c1494ee0f253
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-18 20:56:53 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,7 +1,6 @@
 #Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela:
 
 algo = input('Digite algo: ')
-
 print(f'O tipo primitivo de {algo} é {type(algo)}')
 print(f'{algo} está em maiúsculo? {algo.isupper()}')
 print(f'{algo} tem apenas dígitos? {algo.isdigit()}')
