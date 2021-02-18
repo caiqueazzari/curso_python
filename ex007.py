18e5eaf383e43362ebf326dc778737dfb02d319f
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-18 19:59:16 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,10 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a média.
+
+aluno = str(input('Digite o nome do(a) aluno(a): '))
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+
+s = n1 + n2
+m = s / 2
+
+print(f'A média das notas do(a) {aluno} é de: {m}')
