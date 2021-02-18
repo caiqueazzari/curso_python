b74cb0261443816db7f9580bddab739bfccd29aa
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-18 19:46:49 -0300

    Create ex005.py

---
+++ ex005.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input('Digite um número: '))
+
+print(f'O sucessor do número {n} é {n + 1} e o seu antecessor é {n - 2}.')
