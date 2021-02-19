ad0246f1946963f16d43c95ab3cdb3f5a3d788a6
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-18 23:01:46 -0300

    Create ex013.py

---
+++ ex013.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+s = float(input('Digite o seu salário: '))
+print(f'Parabéns por digitar!\nVocê acaba de receber um aumento de 15%, o seu novo salário é: R${s * 1.15:.2f}')
