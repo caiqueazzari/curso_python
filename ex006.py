635c0cbeeacb27eda209449980aba68d14593831
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-18 22:53:49 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -3,4 +3,4 @@
 n = int(input('Digite um número: '))
 print(f'O dobro de {n} é {n*2}.')
 print(f'O triplo de {n} é {n*3}.')
-print(f'A raiz quadrada de {n} é {n**(1/2):.4}.')
+print(f'A raiz quadrada de {n} é {n**(1/2):.4f}.')
