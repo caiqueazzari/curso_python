c9af12874a8d439caa5f032fb2feceb33afcc7e0
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-18 23:00:10 -0300

    Create ex012.py

---
+++ ex012.py
@@ -0,0 +1,5 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+print('Promoção de 5% aproveite!!')
+p = float(input('Para ganhar o desconto digite o preço do produto: '))
+print(f'O preço do produto com desconto é de: {p * 0.95:.2f}')
