db7c4d9bb2d8d6ec85c704dea56d53bbdab411f5
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-18 21:35:26 -0300

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -1,6 +1,6 @@
 #Escreva um programa que leia um valor em metros e o exiba em centímetros e milímetros.
 
 n = int(input('Digite um valor em metros: '))
-c = n * 100
-m = n * 1000
-print(f'{n}m equivalem {c}cm e {m}mm.')
+cm = n * 100
+mm = n * 1000
+print(f'{n}m equivalem {cm}cm e {mm}mm.')
