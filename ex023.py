45c933e0c84237b6a1677b0bda4e5696002a1baa
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-20 23:15:37 -0300

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -13,4 +13,4 @@ u = num//1%10
 d = num//10%10
 c = num//100%10
 m = num//1000%10
-print(f'Unidade: {u} \nDezena: {d} \nCentena: {c} \nMilhar:{m}')
+print(f'\nUnidade: {u} \nDezena: {d} \nCentena: {c} \nMilhar:{m}')
