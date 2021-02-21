74c88ed73e7f08e6680e34cbbc6b85c721c71d59
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-20 23:21:12 -0300

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,9 +1,12 @@
 '''Crie um programa que leia o nome completo de uma pessoa e mostre:
 - O nome com todas as letras maiúsculas e minúsculas.
 - Quantas letras ao todo (sem considerar espaços).
+- Quantas letras tem o primeiro nome.
 - Quantas letras tem o primeiro nome.'''
 
 name = str(input('Digite o seu nome completo: '))
-print(f'Olá {name}! \nO seu nome com todas as letras maiúsculas: {name.upper().strip()}')
+print(f'Olá {name.strip().capitalize()}! \nO seu nome com todas as letras maiúsculas: {name.upper().strip()}')
 print(f'O seu nome com todas as letras minúsculas: {name.lower().strip()}')
+lista = name.split()
 print(f'O seu nome contém {len(name.strip().replace(" ", ""))} letras!')
+print(f'O seu primeiro nome possui {len(lista[0])} letras!')
