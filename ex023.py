177926332fa78951a64fc3bf57df1cf3e382e926
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-20 23:14:48 -0300

    Create ex023.py

---
+++ ex023.py
@@ -0,0 +1,16 @@
+'''Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados
+
+Ex:
+Digite um número: 1834
+
+unidade: 4
+dezena: 3
+centena: 8
+milhar: 1'''
+
+num = int(input('Digite um número de 0 a 9999: '))
+u = num//1%10
+d = num//10%10
+c = num//100%10
+m = num//1000%10
+print(f'Unidade: {u} \nDezena: {d} \nCentena: {c} \nMilhar:{m}')
