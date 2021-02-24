024723d3b87004ad8ef96421db826a6c0a5f4f02
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-23 21:38:56 -0300

    Update ex028.py

--- ex028.py
+++ ex028.py
@@ -1,6 +1,5 @@
 '''Escreve um programa que faça o computador "pensar" em um número inteiro entre 0 a 5 e peça para o usuário tentar
 descobrir qual foi o número escolhido pelo computador.
-
 O programa deverá escrever na tela se o usuário venceu ou perdeu.'''
 
 import random
