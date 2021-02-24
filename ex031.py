415119ceb5f0c984d6c986b438e1282e7720d740
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-23 22:11:59 -0300

    Create ex031.py

---
+++ ex031.py
@@ -0,0 +1,13 @@
+'''Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem,
+cobrando R$0,50 por Km para viagens de até 200km e R$ 0,45 para viagens mais longas'''
+
+viagem = float(input('Qual foi a distância da viagem em km? '))
+
+if viagem <= 200:
+    topay = viagem * 0.50
+    print(f'O preço a se pagar pela viagem é de R${topay:.2f}.')
+elif viagem >= 201:
+    topay = viagem * 0.45
+    print(f'O preço a se pagar pela viagem é de R${topay:.2f}.')
+else:
+    print('Informação inválida!')
