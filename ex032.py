f11aa7e1146a0fe2479b14f69f1891e0b3d90d45
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-24 18:22:23 -0300

    Create ex032.py

---
+++ ex032.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+
+ano = int(input('Digite um ano: '))
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é BISSEXTO!')
+else:
+    print(f'O ano {ano} não é BISSEXTO!')
