4b887afdd905e16abfb95ada5f72606967bd0192
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-23 22:13:32 -0300

    Update ex031.py

--- ex031.py
+++ ex031.py
@@ -1,7 +1,11 @@
 '''Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem,
 cobrando R$0,50 por Km para viagens de até 200km e R$ 0,45 para viagens mais longas'''
 
+from time import sleep
+
 viagem = float(input('Qual foi a distância da viagem em km? '))
+print('Processando..')
+sleep(3)
 
 if viagem <= 200:
     topay = viagem * 0.50
