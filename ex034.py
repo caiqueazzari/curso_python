eaf4c6d474fa551e0f2867fec94fa8ca3d749384
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-24 23:05:36 -0300

    Update ex034.py

--- ex034.py
+++ ex034.py
@@ -2,12 +2,12 @@
 Para salários superiores a R$1.250,00, calcule um aumento de 10%.
 Para inferiores ou iguais de R$1.250,00. o aumento é de 15%.'''
 
-sal = float(input('\033[036mDigite o valor do seu salário: R$'))
+sal = float(input('\033[36mDigite o valor do seu salário: \033[mR$'))
 
-print('Parabéns, você acaba de receber um aumento!')
+print('\033[36mParabéns, você acaba de receber um aumento!')
 
 if sal > 1250:
-    print(f'O seu novo salário é de: \033[mR${sal * 1.10:.2f}!')
+    print(f'O seu novo salário é de: \033[mR${sal * 1.10:.2f}\033[36m!')
 
 if sal < 1250:
-    print(f'O seu novo salário é de: \033[mR${sal * 1.15:.2f}!')
+    print(f'O seu novo salário é de: \033[mR${sal * 1.15:.2f}\033[36m!')
