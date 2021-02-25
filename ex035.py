6de0c4707579beea56ef8a8041a19bd98f6dad8b
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-24 22:57:46 -0300

    Update ex035.py

--- ex035.py
+++ ex035.py
@@ -1,10 +1,10 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 
-t1 = float(input('Digite o valor da primeira reta: '))
-t2 = float(input('Digite o valor da segunda reta: '))
-t3 = float(input('Digite o valor da terceira reta: '))
+t1 = float(input('\033[31mDigite o valor da primeira reta: '))
+t2 = float(input('\033[31mDigite o valor da segunda reta: '))
+t3 = float(input('\033[31mDigite o valor da terceira reta: '))
 
 if t1 < t2 + t3 and t2 < t1 + t3 and t3 < t1 + t2:
-    print('Os valores acima podem formar um triângulo! ')
+    print('\033[33mOs valores acima podem formar um triângulo! ')
 else:
-    print('Os valores não podem formar um triângulo! ')
+    print('\033[33mOs valores não podem formar um triângulo! ')
