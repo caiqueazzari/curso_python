75034b9359152d8374d3fee2cf189b8bac4615a3
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-02-26 18:41:33 -0300

    Create ex044.py

---
+++ ex044.py
@@ -0,0 +1,25 @@
+'''Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+- À vista dinheiro/cheque: 10% de desconto.
+- À vista no cartão: 5% de desconto.
+- Em até 2x no cartão: preço normal.
+- 3x ou mais no cartão: 20% de juros.'''
+
+preço = str(input('Digite o preço do produto: R$'))
+preço = float(preço.replace(',', '.'))
+pagamento = int(input('Opções de pagamento:'
+                      '\n\t1 - Dinheiro: 10% de desconto'
+                      '\n\t2 - Cheque: 10% de desconto'
+                      '\n\t3 - 1x no cartão: 5% de desconto'
+                      '\n\t4 - 2x no cartão: 0% de desconto'
+                      '\n\t5 - 3x ou mais no cartão: 20% de juros'
+                      '\nDigite a opção de pagamento: '))
+if pagamento == 1 or pagamento == 2:
+    print(f'Valor a ser pago pelo produto: R${preço - (preço * 0.10):.2f}')
+elif pagamento == 3:
+    print(f'Valor a se pagar pelo produto: R${preço - (preço * 0.05):.2f}')
+elif pagamento == 4:
+    print(f'Valor a ser pago pelo produto: 2 parcelas de R${preço / 2:.2f}')
+elif pagamento == 5:
+    print(f'Valor total a ser pago pelo produto: R${preço + (preço * 0.20):.2f}')
+else:
+    print('Opção inválida.')
