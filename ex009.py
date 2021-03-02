fb73e2f59a9b721294d81f88a09fec7b2ef61d5f
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-02 00:01:34 -0300

    Update ex009.py

--- ex009.py
+++ ex009.py
@@ -1,10 +1,9 @@
 #Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 
 n = int(input('Digite um número: '))
-print(f'A tabuada do número {n} é: \n')
+print(f'A tabuada do número {n} é: ')
 x = 0
 
 for x in range(11):
     print(f'{n} x {x:2} = {n * x}')
     x + 1
-
