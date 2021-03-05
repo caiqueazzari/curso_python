ed2f6ceeee5c988e781072fd26a81ea9a81bb503
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-05 20:19:18 -0300

    Create ex049.py

---
+++ ex049.py
@@ -0,0 +1,8 @@
+#Refaça o DESAFIO 09, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando o laço for.
+
+n = int(input('Digite um número inteiro: '))
+print(f'A tabuada de {n} é: ')
+
+for x in range(0, 11):
+    print(n, 'x', x, ':', n * x)
+
