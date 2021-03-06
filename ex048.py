4ccf8f1667bf0b21aca3edec598895d717289cd4
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-06 20:40:04 -0300

    Update ex048.py

--- ex048.py
+++ ex048.py
@@ -3,7 +3,7 @@
 soma = 0
 
 for x in range(1, 501, 2):
-    print(x)
-    soma += x
-    
-print(f'A soma de todos os números ímpares de 1 até 500 é de {soma}.')
+    if x % 3 == 0:
+        soma += x
+
+print(f'A soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500 é de {soma}.')
