b6eacfa3d511a636a8a1f82434bdef2f1e873717
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-09 08:42:49 -0300

    Update ex053.py

--- ex053.py
+++ ex053.py
@@ -1,8 +1,10 @@
 '''Crie um programa que leia uma frase qualquer e diga se ela é palíndromo, desconsiderando os espaços.
 Ex: Apos a sopa'''
 
-frase = str(input('Digite uma frase: ')).replace(' ', '')
+frase = str(input('Digite uma frase: ')).replace(' ', '').upper()
 frase_invertida = frase[::-1]
+
+print(f'O inverso dessa frase é {frase_invertida}.')
 
 if frase == frase_invertida:
     print('A frase é um palíndromo! ')
