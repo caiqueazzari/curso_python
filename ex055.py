2497d61a02732d8f0e88fc34deb9f0eb609a3185
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-09 10:11:58 -0300

    Update ex055.py

--- ex055.py
+++ ex055.py
@@ -1,9 +1,17 @@
 #Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
-peso = []
+menor = 0
+maior = 0
 
 for x in range(1, 6):
-    peso.append(float(input(f'Digite o peso da {x}º pessoa: ')))
+    peso = float(input(f'Digite o peso da {x}º pessoa: '))
+    if x == 1:
+        menor = peso
+        maior = peso
+    else:
+        if peso > maior:
+            maior = peso
+        elif peso < menor:
+            menor = peso
 
-peso = sorted(peso)
-print(f'O maior peso foi de {peso[0]} e o menor foi de {peso[4]}')
+print(f'O maior peso foi de {maior:.1f}kg \nO menor foi de {menor:.1f}kg')
