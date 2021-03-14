ec5186bee7447d89e59af82ffb6d0de7f48dab64
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-13 21:49:45 -0300

    Update ex057.py

--- ex057.py
+++ ex057.py
@@ -1,10 +1,10 @@
 '''Faça um programa que leia o sexo de uma pessoa, mas só aceite valores 'M' ou 'F' caso esteja errado,
 peça a digitação novamente até ter um valor correto.'''
 
-sexo = str(input('Digite o seu sexo: (M/F) ')).strip().upper()
+sexo = str(input('Digite o seu sexo: (M/F) ')).strip().upper()[0]
 
 while sexo not in 'MF':
     print('Dados inválidos! ')
-    sexo = str(input('Digite o seu sexo: (M/F) ')).strip().upper()
+    sexo = str(input('Digite o seu sexo: (M/F) ')).strip().upper()[0]
 
 print(f'Sexo {sexo} registrado com sucesso.')
