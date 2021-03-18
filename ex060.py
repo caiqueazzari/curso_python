d96e411f2b36344edf42eec94c4b2fa541b77861
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-18 18:37:28 -0300

    Update ex060.py

--- ex060.py
+++ ex060.py
@@ -3,7 +3,13 @@
 Ex:
 5! = 5 x 4 x 3 x 2 x 1 = 120'''
 
-from math import factorial
-
-num = int(input('Digite um número: '))
-print(f'{num}! = {factorial(num)}')
+n = int(input('Digite um número: '))
+c = n
+f = 1
+print(f'{n}! = ', end='')
+while c > 0:
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print(f'{f}')
