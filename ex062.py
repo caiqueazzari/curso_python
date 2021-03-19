ccac7fe2a8d9fec22949a8ee224fa43382d63f9f
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-19 18:35:00 -0300

    Update ex062.py

--- ex062.py
+++ ex062.py
@@ -1,5 +1,7 @@
-'''Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
-O programa encerrará quando ele disser que quer mostrar 0 termos.'''
+'''
+Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
+O programa encerrará quando ele disser que quer mostrar 0 termos.
+'''
 
 n1 = int(input('Digite o \033[1;36m1º\033[0;38m termo da PA: '))
 n = int(input('Digite o \033[1;36mN-ésimo\033[0;38m termo da PA: '))
