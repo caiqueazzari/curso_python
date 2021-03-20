b7b8ae015258f34d033d573325d2c4ed9a4814cf
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-20 20:13:26 -0300

    Update ex064.py

--- ex064.py
+++ ex064.py
@@ -4,12 +4,13 @@ o valor 999, que é a condição de parada. No final, mostre quantos números fo
 
 cont, x, soma, num = 0, 1, 0, 0
 print('\n[Digite 999 caso queira parar de adicionar números.]')
+num = int(input(f'\nDigite o {x}° número inteiro: '))
 
 while num != 999:
-    num = int(input(f'\nDigite o {x}° número inteiro: '))
     x += 1
     cont += 1
     soma += num
+    num = int(input(f'\nDigite o {x}° número inteiro: '))
 
 print(f'\nQuantidade de números digitados: {cont}.')
-print(f'\nA soma de todos os {cont - 1} números digitados é de {soma - 999}.')
+print(f'\nA soma de todos os {cont} números digitados é de {soma}.')
