efbd0430f4ca41248d2794e4b8b0347b0531afa8
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-03-21 19:32:32 -0300

    Update ex068.py

--- ex068.py
+++ ex068.py
@@ -14,32 +14,29 @@ consec = 0
 
 while True:
     num_p = int(input('\nDigite um valor: '))
-    escolha_p = str(input('\nPar ou Impar? ')).strip().upper()[0]
+    escolha_p = ' '
+    while escolha_p not in 'PIÍ':
+        escolha_p = str(input('\nPar ou Impar? ')).strip().upper()[0]
     print('\nProcessando..')
     sleep(2)
     num_c = randint(1, 10)
     resultado = (num_c + num_p) % 2
+    print(f'\nSeu número: {num_p}\n\nNúmero do computador: {num_c} ', end='')
+    print(f'\n\n{num_p} + {num_c} = {num_c + num_p} = Par. ' if resultado == 0
+          else f'\n\n{num_p} + {num_c} = {num_c + num_p} = Ímpar.')
 
     if escolha_p == 'I' or escolha_p == 'Í':
-        escolha_c = 'P'
         if resultado == 1:
-            print(f'\nSeu número: {num_p}\n\nNúmero do computador: {num_c}\n\n{num_p} + {num_c} = {num_c + num_p} = Ímpar')
             print(f'\n\033[1;32mVocê ganhou!\033[0;38m ')
         else:
-            print(f'\nSeu número: {num_p}\n\nNúmero do computador: {num_c}\n\n{num_p} + {num_c} = {num_c + num_p} = Par')
             print('\n\033[1;31mVocê perdeu!\033[0;38m \n')
             break
     elif escolha_p == 'P':
-        escolha_c = 'I'
         if resultado == 0:
-            print(f'\nSeu número: {num_p}\n\nNúmero do computador: {num_c}\n\n{num_p} + {num_c} = {num_c + num_p} = Par')
             print(f'\n\033[1;32mVocê ganhou!\033[0;38m ')
         else:
-            print(f'\nSeu número: {num_p}\n\nNúmero do computador: {num_c}\n\n{num_p} + {num_c} = {num_c + num_p} = Ímpar')
             print(f'\n\033[1;31mVocê perdeu! \033[0;38m\n')
             break
-    else:
-        print('\n\033[1;31mDigite uma escolha válida! \033[0;38mm')
 
     consec += 1
 
