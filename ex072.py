daf1f1c67c7d0a2c1be4157ec0e0a84a1f05d8f6
unknown <caiquetake@gmail.com>
2021-03-23 15:55:54 -0300

    Criando ex072.py

---
+++ ex072.py
@@ -0,0 +1,9 @@
+'''
+Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+Seu programa deverá ler um número pelo teclado(entre 0 e 20) e mostrá-lo por extenso.
+'''
+num_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+                'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+num = int(input('\nDigite um número entre 0 e 20: '))
+print(f'\nO número {num} por extenso é: {num_extenso[num]}')
