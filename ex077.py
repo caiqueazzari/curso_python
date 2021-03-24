05fcf508a47f52891a5c509cc554a95d3671e19b
caiqueazzari <caiquetake@gmail.com>
2021-03-24 11:06:38 -0300

    Update

--- ex077.py
+++ ex077.py
@@ -5,21 +5,9 @@ Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
 palavras = ('cachorro', 'caderno', 'abacaxi', 'arroz', 'carne', 'bola', 'gato', 'notenook')
 
-for x in range(len(palavras)):
-    print(f'\nVogais da palavra {palavras[x]}: ', end='')
-    a = palavras[x].count('a')
-    e = palavras[x].count('e')
-    i = palavras[x].count('i')
-    o = palavras[x].count('o')
-    u = palavras[x].count('u')
-    if a >= 1:
-        print('a ', end='')
-    if e >= 1:
-        print('e ', end='')
-    if i >= 1:
-        print('i ', end='')
-    if o >= 1:
-        print('o ', end='')
-    if u >= 1:
-        print('u ', end='')
+for x in palavras:
+    print(f'\nVogais da palavra {x}: ', end='')
+    for y in x:
+        if y in 'aeiou':
+            print(y, end=' ')
     print()
