fdd544a4f6c71bf0c5d686dde239c986defd73dd
Caique Azzari <caiquetake@gmail.com>
2021-04-01 11:03:52 -0300

    Update ex082.py

--- ex082.py
+++ ex082.py
@@ -4,22 +4,18 @@ Depois disso, crie duas listas extras que vão conter apenas os valores pares e 
 respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 '''
 
-valores = list()
-par = list()
-impar = list()
-resp = ' '
+valores, par, impar, resp, x = list(), list(), list(), ' ', 0
 
 while resp != 'N':
     resp = ' '
     valores.append(int(input('Digite um valor: ')))
-    while resp not in 'SN':
-        resp = str(input('Deseja continuar? (S/N) ')).strip().upper()[0]
-
-for x in range(0, len(valores)):
     if valores[x] % 2 == 0:
         par.append(valores[x])
     else:
         impar.append(valores[x])
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar? (S/N) ')).strip().upper()[0]
+    x += 1
 
 print(f'Valores digitados: {valores}')
 print(f'Valores pares digitados: {par}')
