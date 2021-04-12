eda45fc61fd37e8f65b9d195a1e30ccd29e47257
Caique Azzari <caiquetake@gmail.com>
2021-04-12 17:01:27 -0300

    Updated

--- ex109/ex109.py
+++ ex109/ex109.py
@@ -1,6 +1,6 @@
 '''
-Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar os números como
-um valor monetário formatado.
+Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor
+retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 '''
 
 from moeda import *
