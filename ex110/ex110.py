80d3ebb6dc6f13f9963045dabacc29ea2cb757cb
Caique Azzari <caiquetake@gmail.com>
2021-04-12 17:23:06 -0300

    Created

---
+++ ex110/ex110.py
@@ -0,0 +1,9 @@
+'''
+Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas
+informações geradas pelas funções que já temos no módulo criado até aqui.
+'''
+
+from moeda import *
+
+preco = float(input('Digite o preço do produto: '))
+resumo(preco, 20, 10)
---
+++ ex110/moeda.py
@@ -0,0 +1,56 @@
+def aumentar(preco=0, taxa=0, format=False):
+    """
+    Essa função aumenta o preço de um produto, com uma taxa de x%.
+    preco = preço do produto
+    taxa = taxa em %
+    format = formatar para um tipo monetário
+    """
+    res = preco + (preco * taxa/100)
+    return res if format is False else monetário(res)
+
+
+def diminuir(preco=0, taxa=0, formato=False):
+    """
+    Essa função diminui o preço de um produto, com uma taxa de x%.
+    preco = preço do produto
+    taxa = taxa em %
+    format = formatar para um tipo monetário
+    """
+    res = preco - (preco * taxa/100)
+    return res if format is False else monetário(res)
+
+
+def metade(preco=0, formato=False):
+    """
+    Essa função mostra a metade do preço de um produto
+    preco = preço do produto
+    format = formatar para um tipo monetário
+    """
+    res = preco / 2
+    return res if not formato else monetário(res)
+
+
+def dobro(preco=0, formato=False):
+    """
+    Essa função mostra a metade do preço de um produto
+    preco = preço do produto
+    format = formatar para um tipo monetário
+    """
+    res = preco * 2
+    return res if not formato else monetário(res)
+
+
+def monetário(preco: float = 0, moeda='R$'):
+    return f'{moeda}{preco:.2f}'.replace('.', ',')
+
+
+def resumo(preco=0, aumento=0, reducao=0):
+    print('-' * 30)
+    print(f'{"Resumo do valor":^30}')
+    print('-' * 30)
+    print(f'Preço analisado: {monetário(preco):>13}')
+    print(f'Dobro do preço: {dobro(preco, True):>13}')
+    print(f'Metade do preço: {metade(preco, True):>13}')
+    print(f'{aumento}% de aumento: {aumentar(preco, aumento, True):>13}')
+    print(f'{reducao}% de desconto: {diminuir(preco, reducao, True):>13}')
+    print('-' * 30)
